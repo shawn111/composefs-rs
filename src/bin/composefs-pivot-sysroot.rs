9f63bd39ff5202ed3a4fd0ec326467d0e0615ee6
Colin Walters <walters@verbum.org>
2024-10-31 08:45:41 -0400

    pivot: Add a unit test

--- src/bin/composefs-pivot-sysroot.rs
+++ src/bin/composefs-pivot-sysroot.rs
@@ -4,8 +4,7 @@ use anyhow::{bail, Result};
 
 use composefs_experiments::{fsverity::Sha256HashValue, repository::Repository};
 
-fn parse_composefs_cmdline() -> Result<Sha256HashValue> {
-    let cmdline = std::fs::read("/proc/cmdline")?;
+fn parse_composefs_cmdline(cmdline: &[u8]) -> Result<Sha256HashValue> {
     // TODO?: officially we need to understand quoting with double-quotes...
     for part in cmdline.split(|c| c.is_ascii_whitespace()) {
         if let Some(digest) = part.strip_prefix(b"composefs=") {
@@ -19,8 +18,25 @@ fn parse_composefs_cmdline() -> Result<Sha256HashValue> {
 
 fn main() -> Result<()> {
     let repo = Repository::open_system()?;
-    let image = parse_composefs_cmdline()?;
+    let cmdline = std::fs::read("/proc/cmdline")?;
+    let image = parse_composefs_cmdline(&cmdline)?;
     repo.pivot_sysroot(&hex::encode(image), Path::new("/sysroot"))?;
 
     Ok(())
+}
+
+#[test]
+fn test_parse() {
+    let failing = ["", "foo", "composefs", "composefs=foo"];
+    for case in failing {
+        assert!(parse_composefs_cmdline(case.as_bytes()).is_err());
+    }
+    let digest = "8b7df143d91c716ecfa5fc1730022f6b421b05cedee8fd52b1fc65a96030ad52";
+    let digest_bytes = hex::decode(&digest).unwrap();
+    assert_eq!(
+        parse_composefs_cmdline(format!("composefs={digest}").as_bytes())
+            .unwrap()
+            .as_slice(),
+        &digest_bytes
+    );
 }
